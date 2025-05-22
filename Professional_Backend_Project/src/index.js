c25e675121ec19ce6c75751cdc0785d11e3f9d05
sunnyrajendraraj <sunny.2201108cs@iiitbh.ac.in>
2025-05-22 23:16:32 +0530

    MongoDB connection (2-Methods)

---
+++ Professional_Backend_Project/src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config({path : './env'})
+import dotenv from 'dotenv'
+import connectDB from './db/index.db.js';
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+/*
+
+import mongoose from 'mongoose'
+import { DB_NAME } from '../constants'
+
+import express from 'express';
+import { error } from 'console';
+const app = express();
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        // Listners : Immedietely after database connnection
+        
+        app.on("error", (error)=>{
+            console.log("ERROR ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is running  on port ${PORT}`)
+        })
+
+    }catch(error){
+        console.error("ERROR: ", error);
+        throw error
+    }
+})()
+
+*/
